a5599e777c977b27bc5cf5c8e10c0afbf5ce60bf
jiilan-create <66149604+jiilan-create@users.noreply.github.com>
2020-06-06 16:32:49 +0700

    Add files via upload

---
+++ Cie.py
@@ -0,0 +1,10 @@
+import sys,time
+def jalan (kata):
+    for e in kata:
+       sys.stdout.write(e)
+       sys.stdout.flush()
+       time.sleep(0.3)
+jalan("Ehm Tunggu sebelum aku bilang ini,aku mau bilang maaf atas semua kesalahan ku".center(44))
+jalan("dah sekarang aku mulai Obrolannya oke"(44))
+jalan("aku sama kamu udah kenal cukup lama aku nyaman sama kamu tapi,aku ngerasa aneh aja ".center(44))
+jalan("Dan sekarang to the point aja mau gak kamu jadi kekasih hatiku" (44))
